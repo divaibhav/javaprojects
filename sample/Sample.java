66c02272af92d6522b6d74b6aa8ea4f033c0d5c6
Vaibhav Diwan <divaibhav@gmail.com>
2024-07-05 17:05:53 +0530

    first commit

---
+++ sample/Sample.java
@@ -0,0 +1,8 @@
+class Sample{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        int maxRange = Integer.MAX_VALUE;
+        int minRange = Integer.MIN_VALUE;
+    System.out.println(minRange + " - 0 - " + maxRange);
+    }
+}
---
+++ variabledeclaration/VariableDemo.java
@@ -0,0 +1,28 @@
+//WAP to display marks, age, section of a student
+class VariableDemo{
+    public static void main(String[] args){
+        // declare variable
+        // float or double
+        double marks;
+        int age;
+        char section;
+        // initialize
+        // identifier = value or literal;
+        marks = 40.5;
+        age = 40;
+        section = 'C';
+        //declare and initialize an String variable
+        String name = "Vaibhav Diwan";
+
+        // printing variables values of console/output
+        System.out.println("Marks = " + marks );
+        System.out.println();
+        System.out.println("Age = " + age); 
+        System.out.println();
+        System.out.println("Section = " + section);
+        System.out.println();
+        System.out.println(name);
+    }
+}
+
+
