a5ac1a43bf55679f302890dc036f3bb25d01d043
Vaibhav Diwan <divaibhav@gmail.com>
2024-07-16 20:32:07 +0530

    class discussion code

--- conditionalstatement/GreatestOfThreeNumbers.java
+++ conditionalstatement/GreatestOfThreeNumbers.java
@@ -3,9 +3,15 @@ class GreatestOfThreeNumbers{
         //to run thiscode type java GreatestOfThreeNumbers 100 300 20
         // 100 300 20 are the arguments, coming to 0,1,2 index of args array
         // this code is show the use of String[] args
-        int number1 = Integer.parseInt(args[0]);//using Integer.parseInt() method to parse(convert) String to integer
-        int number2 = Integer.parseInt(args[1]);
-        int number3 = Integer.parseInt(args[2]);
+        String firstArgument = args[0];// "112" "q12"
+        String secondArgument = args[1];
+        String thirdArgument = args[2];
+        int number1 = Integer.parseInt(firstArgument);//using Integer.parseInt() method to parse(convert) String to integer
+        int number2 = Integer.parseInt(secondArgument);
+        int number3 = Integer.parseInt(thirdArgument);
+        var name = "123";
+        var surName = 1;
+        System.out.println(name + surName);
 
         if(number1 > number2){
             if(number1 > number3) {
